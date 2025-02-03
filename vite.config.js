0646a3e09ea64938681b846d04b5f89935209aed
Albert~M <105120358+Master-Chief131@users.noreply.github.com>
2025-02-03 00:20:03 -0500

    Add grid SVG file and update Vite configuration for manual chunking

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        // Definir manualmente los chunks para dividir el código
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    },
+    // Ajustar el límite de advertencia de tamaño de chunk a 1000 kB
+    // chunkSizeWarningLimit: 1000,
+  },
+});
