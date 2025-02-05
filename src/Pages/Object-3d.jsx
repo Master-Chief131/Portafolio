79d2a323566f39bf9128b6fb449de532a47d554b
Albert~M <105120358+Master-Chief131@users.noreply.github.com>
2025-02-05 15:28:59 -0500

    Remove commented-out code from Object3D component

--- src/Pages/Object-3d.jsx
+++ src/Pages/Object-3d.jsx
@@ -5,21 +5,6 @@ import { Link } from "react-router-dom";
 const Object3D = () => {
   return (
     <Spline scene="https://prod.spline.design/niRx4i1tDLyn-RdG/scene.splinecode" />
-    // <div className="min-h-screen flex items-center justify-center">
-    //   <div className="absolute top-4 left-4 inline-flex items-center justify-center px-4 py-2 ">
-    //     <Link
-    //       to="/"
-    //       className="absolute top-4 left-4 inline-flex items-center justify-center px-4 py-2 bg-gradient-to-r from-[#6366f1] to-[#a855f7] text-white rounded-xl font-semibold transition-all duration-300 hover:scale-[1.02] hover:shadow-lg hover:shadow-[#6366f1]/20 active:scale-[0.98]"
-    //     >
-    //       Regresar
-    //     </Link>
-    //   </div>
-    //   <div className="flex justify-center mb-6">
-    //     <Suspense fallback={<div>Cargando modelo...</div>}>
-    //       <Spline scene="https://prod.spline.design/niRx4i1tDLyn-RdG/scene.splinecode" />
-    //     </Suspense>
-    //   </div>
-    // </div>
   );
 }
 
